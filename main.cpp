937330746817151dabc17ef78399c24ab48ec5da
AECC1234 <3208541431@qq.com>
2025-08-15 13:58:43 +0800

    Resloved which combination to generate the strings!

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char* argv[])
 	int mode = 0;
 	int col = 100;
 	char* word = new char[WORD_IN_EACH_LINE]();
-	char* path_to_save[260] = "";
+	char path_to_save[260] = "";
 	time_t t = 0;
 	bool tosave = false;
 	const char* args[4] = {
@@ -22,32 +22,33 @@ int main(int argc, char* argv[])
 		"-lines",
 	};
 	
-	for (int i = 0; i < 10, i++)
+	for (int i = 0; i < 10; i++)
 	{
 		if (strcmp(argv[i], args[0]) == 0)
 		{
-			switch (argv[i + 1])
+			if (strcmp(argv[i + 1], "a") == 0)
 			{
-			case "a":
 				mode = 0;
-				break;
-			case "A":
+			}
+			if (strcmp(argv[i + 1], "A") == 0)
+			{
 				mode = 1;
-				break;
-			case "0":
+			}
+			if (strcmp(argv[i + 1], "0") == 0)
+			{
 				mode = 2;
-				break;
-			case "aA":
+			}
+			if (strcmp(argv[i + 1], "aA") == 0)
+			{
 				mode = 3;
-				break;
-			case "A0":
+			}
+			if (strcmp(argv[i + 1], "A0") == 0)
+			{
 				mode = 4;
-				break;
-			case "aA0":
+			}
+			if (strcmp(argv[i + 1], "aA0") == 0)
+			{
 				mode = 5;
-				break;
-			default:
-				continue;
 			}
 		}
 		else if (strcmp(argv[i], args[1]) == 0)
@@ -74,7 +75,30 @@ int main(int argc, char* argv[])
 	{
 		for (int i = 0; i < WORD_IN_EACH_LINE - 1; i++)
 		{
-			m_switch = rand() % 3;
+			switch (mode)
+			{
+			case 0:
+				m_switch = 0;
+				break;
+			case 1:
+				m_switch = 1;
+				break;
+			case 2:
+				m_switch = 2;
+				break;
+			case 3:
+				m_switch = rand() % 2;
+				break;
+			case 4:
+				m_switch = rand() % 2 + 1;
+				break;
+			case 5:
+				m_switch = rand() % 3;
+				break;
+			default:
+				break;
+			}
+
 			switch (m_switch)
 			{
 			case 0:
