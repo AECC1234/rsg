fc9304248e23fcff303de34f29732ecbf93b571b
AECC1234 <3208541431@qq.com>
2025-08-15 14:39:16 +0800

    Fixed some bugs!

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char* argv[])
 	FILE* fp = 0;
 	int m_switch = 0;
 	int len = 200;
-	int mode = 0;
+	int mode = 5;
 	int col = 100;
 	char* word = new char[WORD_IN_EACH_LINE]();
 	char write[100] = "";
@@ -23,31 +23,32 @@ int main(int argc, char* argv[])
 		"-lines",
 	};
 	
-	for (int i = 0; i < 10; i++)
+	for (int i = 1; i < 11; i++)
 	{
+		if (argv[i] == NULL) break;
 		if (strcmp(argv[i], args[0]) == 0)
 		{
 			if (strcmp(argv[i + 1], "a") == 0)
 			{
 				mode = 0;
 			}
-			if (strcmp(argv[i + 1], "A") == 0)
+			else if (strcmp(argv[i + 1], "A") == 0)
 			{
 				mode = 1;
 			}
-			if (strcmp(argv[i + 1], "0") == 0)
+			else if (strcmp(argv[i + 1], "0") == 0)
 			{
 				mode = 2;
 			}
-			if (strcmp(argv[i + 1], "aA") == 0)
+			else if (strcmp(argv[i + 1], "aA") == 0)
 			{
 				mode = 3;
 			}
-			if (strcmp(argv[i + 1], "A0") == 0)
+			else if (strcmp(argv[i + 1], "A0") == 0)
 			{
 				mode = 4;
 			}
-			if (strcmp(argv[i + 1], "aA0") == 0)
+			else if (strcmp(argv[i + 1], "aA0") == 0)
 			{
 				mode = 5;
 			}
