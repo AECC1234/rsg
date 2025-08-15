32306f6ca38447fd2ca47229a3a88a9473fea36b
AECC1234 <3208541431@qq.com>
2025-08-15 13:35:18 +0800

    #1 Add an args detection function!

--- main.cpp
+++ main.cpp
@@ -1,21 +1,73 @@
 #include <iostream>
+#include <cstring>
 #include <math.h>
 using namespace std;
 #define WORD_IN_EACH_LINE 100
 
 int main(int argc, char* argv[])
 {
+	FILE* fp = 0;
 	int m_switch = 0;
 	int len = 200;
+	int mode = 0;
+	int col = 100;
 	char* word = new char[WORD_IN_EACH_LINE]();
+	char* path_to_save[260] = "";
 	time_t t = 0;
-	const char* args[5] = {
+	bool tosave = false;
+	const char* args[4] = {
 		"-mode",
 		"-option",
 		"-save",
 		"-lines",
-		"-coloms"
 	};
+	
+	for (int i = 0; i < 10, i++)
+	{
+		if (strcmp(argv[i], args[0]) == 0)
+		{
+			switch (argv[i + 1])
+			{
+			case "a":
+				mode = 0;
+				break;
+			case "A":
+				mode = 1;
+				break;
+			case "0":
+				mode = 2;
+				break;
+			case "aA":
+				mode = 3;
+				break;
+			case "A0":
+				mode = 4;
+				break;
+			case "aA0":
+				mode = 5;
+				break;
+			default:
+				continue;
+			}
+		}
+		else if (strcmp(argv[i], args[1]) == 0)
+		{
+
+		}
+		else if (strcmp(argv[i], args[2]) == 0)
+		{
+			strcpy(path_to_save, argv[i + 1]);
+			tosave = true;
+		}
+		else if (strcmp(argv[i], args[3]) == 0)
+		{
+			len = stoi(argv[i + 1], 0, 10);
+		}
+		else
+		{
+
+		}
+	}
 
 	srand((unsigned)time(&t));
 	for (size_t i = 0; i < len; i++)
